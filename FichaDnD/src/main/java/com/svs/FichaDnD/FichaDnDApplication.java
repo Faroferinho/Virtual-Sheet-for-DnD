80b881b27034d0d4e437f10b847a386bae568c7d
Faroferinho <conrado.perini.fracacio@gmail.com>
2025-02-03 15:41:58 -0300

    Criação do projeto em Thymeleaf

---
+++ FichaDnD/src/main/java/com/svs/FichaDnD/FichaDnDApplication.java
@@ -0,0 +1,13 @@
+package com.svs.FichaDnD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FichaDnDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FichaDnDApplication.class, args);
+	}
+
+}
---
+++ FichaDnD/src/test/java/com/svs/FichaDnD/FichaDnDApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svs.FichaDnD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FichaDnDApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
